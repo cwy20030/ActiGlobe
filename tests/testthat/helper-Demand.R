d829da5698077bd6f39c01d08e7b957c6687ffd3
C. William Yao <35785225+cwy20030@users.noreply.github.com>
2025-12-10 23:29:44 -0500

    Add test for Demand function with mock input
    Signed-off-by: C. William Yao <35785225+cwy20030@users.noreply.github.com>

--- tests/testthat/helper-Demand.R
+++ tests/testthat/helper-Demand.R
@@ -9,3 +9,14 @@ test_that("Demand returns 1 when user selects 1", {
          
 })
 
+
+test_that("Demand returns Other when user selects 2", {
+  
+  # mock readline to always return "3"
+  mockery::stub(Demand, "readline", function(prompt = "") "2")
+
+  result <- Demand(c("NOOOO", "Yes", "Other"), "option")
+
+  # ---- Content checks ----
+  expect_equal(result, "Yes")
+})
